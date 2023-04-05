80c321f35b0a258c0c1c3afc6ad9c69555c13f2a
cbkako <kasperi.kouri@tuni.fi>
2023-04-05 13:42:11 +0300

    Add 4.5

--- osa_4/back/utils/list_helper.js
+++ osa_4/back/utils/list_helper.js
@@ -9,7 +9,27 @@ const totalLikes = (blogs) => {
   return blogs.reduce((currentLikes, blog) => currentLikes + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  return blogs.reduce((prev, current) =>
+    prev.likes > current.likes
+      ? {
+          title: prev.title,
+          author: prev.author,
+          likes: prev.likes,
+        }
+      : {
+          title: current.title,
+          author: current.author,
+          likes: current.likes,
+        }
+  )
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
