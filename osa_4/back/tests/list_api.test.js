f606a4aeb3c706803d8f3b9632e6d3d2afda4198
cbkako <kasperi.kouri@tuni.fi>
2023-04-06 21:24:09 +0300

    Add 4.9

--- osa_4/back/tests/list_api.test.js
+++ osa_4/back/tests/list_api.test.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
+const logger = require('../utils/logger')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog))
+  const promiseArray = blogObjects.map((blog) => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test("Object contain key 'id' instead of '_id'", async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+  expect(blog.id).toBeDefined()
 })
 
 afterAll(async () => {
